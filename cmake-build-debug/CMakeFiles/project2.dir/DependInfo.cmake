c84a38b6edc142d300fe7dfb22589e4c7b0d9861
Yael Magid <yael11072@gmail.com>
2019-01-07 22:59:54 +0200

    initalize

---
+++ cmake-build-debug/CMakeFiles/project2.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yael/CLionProjects/project2/CacheManager.cpp" "/home/yael/CLionProjects/project2/cmake-build-debug/CMakeFiles/project2.dir/CacheManager.cpp.o"
+  "/home/yael/CLionProjects/project2/ClientHandler.cpp" "/home/yael/CLionProjects/project2/cmake-build-debug/CMakeFiles/project2.dir/ClientHandler.cpp.o"
+  "/home/yael/CLionProjects/project2/FileCacheManager.cpp" "/home/yael/CLionProjects/project2/cmake-build-debug/CMakeFiles/project2.dir/FileCacheManager.cpp.o"
+  "/home/yael/CLionProjects/project2/MySerialServer.cpp" "/home/yael/CLionProjects/project2/cmake-build-debug/CMakeFiles/project2.dir/MySerialServer.cpp.o"
+  "/home/yael/CLionProjects/project2/MyTestClientHandler.cpp" "/home/yael/CLionProjects/project2/cmake-build-debug/CMakeFiles/project2.dir/MyTestClientHandler.cpp.o"
+  "/home/yael/CLionProjects/project2/Solver.cpp" "/home/yael/CLionProjects/project2/cmake-build-debug/CMakeFiles/project2.dir/Solver.cpp.o"
+  "/home/yael/CLionProjects/project2/main.cpp" "/home/yael/CLionProjects/project2/cmake-build-debug/CMakeFiles/project2.dir/main.cpp.o"
+  "/home/yael/CLionProjects/project2/server_side.cpp" "/home/yael/CLionProjects/project2/cmake-build-debug/CMakeFiles/project2.dir/server_side.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/project2.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project2.dir/main.cpp.o"
+  "CMakeFiles/project2.dir/server_side.cpp.o"
+  "CMakeFiles/project2.dir/MySerialServer.cpp.o"
+  "CMakeFiles/project2.dir/ClientHandler.cpp.o"
+  "CMakeFiles/project2.dir/MyTestClientHandler.cpp.o"
+  "CMakeFiles/project2.dir/Solver.cpp.o"
+  "CMakeFiles/project2.dir/CacheManager.cpp.o"
+  "CMakeFiles/project2.dir/FileCacheManager.cpp.o"
+  "project2.pdb"
+  "project2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
