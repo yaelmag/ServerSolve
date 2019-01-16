3ffae4fcc741e592b4a482556bd31a041dd02fed
adiyanai <adiyanai10@gmail.com>
2019-01-16 14:57:09 +0200

    update :)

--- ClientHandler.h
+++ ClientHandler.h
@@ -12,7 +12,7 @@ class ClientHandler {
 public:
     virtual void handleClient(int cliSocket) = 0;
 
-    virtual CacheManager* getCache() {}
+    virtual CacheManager* getCache() = 0;
 
     virtual ~ClientHandler() = default;
 };
